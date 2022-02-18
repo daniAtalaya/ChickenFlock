3f0ca8b3fd6458b4a7a3251e35c8412304d7269d
daniAtalaya <daniel.atalaya.nina@gmail.com>
2022-02-18 08:29:04 +0100

    Correct

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <SDL.h>
+int main(int argc, char* argv[]) { SDL_Event e;
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) return -1;
+    if (!SDL_CreateWindow("Daniel Atalaya's First SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0)) return -1;
+    for (;;) while (SDL_PollEvent(&e) > 0) if (e.type == SDL_QUIT) return 0; }
+/*#include <iostream>
+#include <SDL.h>
+int main(int argc, char* argv[]) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        std::cout << "Failed to initialize the SDL2 library" << std::endl << "SDL2 Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    SDL_Window* window = SDL_CreateWindow("Daniel Atalaya's First SDL2 Window", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 680, 480, 0);
+    if (!window) {
+        std::cout << "Failed to create window" << std::endl << "SDL2 Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    SDL_Surface* window_surface = SDL_GetWindowSurface(window);
+    if (!window_surface) {
+        std::cout << "Failed to get the surface from the window" << std::endl << "SDL2 Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    SDL_Surface* image = SDL_LoadBMP("guerrilla-war.bmp");
+    if (!image) {
+        std::cout << "Failed to load image" << std::endl << "SDL2 Error: " << SDL_GetError() << std::endl;
+        return -1;
+    }
+    while (true) {
+        SDL_Event e;
+        while (SDL_PollEvent(&e) > 0) {
+            switch (e.type) {
+                case SDL_QUIT:
+                    return 0;
+            }
+            SDL_BlitSurface(image, NULL, window_surface, NULL);
+            SDL_UpdateWindowSurface(window);
+        }
+    }
+    return 0;
+}*/
